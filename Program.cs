e2e3c98c420fe0c3c11b1207e07d15a70fda27fe
Nicolas Rocha Lacerda <94977120+NRLacerda@users.noreply.github.com>
2024-01-14 23:59:21 -0300

    feat(aws):integration with sqs post message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿using Amazon.SQS;
+using UserPublisher;
+using UserPublisher.Messages;
+using Newtonsoft.Json;
 
-var sqsClient = new AmazonSQSClient(region:Amazon.RegionEndpoint.USEast1);
+
+//O AWSconfig app deve estar no mesmo diretório de program.cs
+
+string jsonFilePath = "Ponha aqui o fullPath do awsConfigApp.json";
+string jsonContent = File.ReadAllText(jsonFilePath);
+dynamic config = JsonConvert.DeserializeObject(jsonContent);
+
+string accessKey = config.AWSAccessKey;
+string secretKey = config.AWSSecretKey;
+string queueName = config.QueueName;
+
+var sqsClient = new AmazonSQSClient(
+    awsAccessKeyId: accessKey,
+    awsSecretAccessKey: secretKey,
+    region: Amazon.RegionEndpoint.USEast1
+);
+
+var sqsPublisher = new SqsPublisher(sqsClient);
+await sqsPublisher.PublishAsync(queueName, new UserCreated
+{
+    UserID = 1,
+    UserName = "teste",
+    Email = "queueName@gmail.com"
+});
+Console.ReadLine();
---
+++ SqsPublisher.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace UserPublisher
+{
+    public class SqsPublisher
+    {
+        private readonly IAmazonSQS _sqs;
+        public SqsPublisher(IAmazonSQS sqs)
+        {
+            _sqs = sqs;
+
+        }
+        public async Task PublishAsync<T> (string queueName, T message)
+        {
+            var queueUrl = await _sqs.GetQueueUrlAsync(queueName);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = queueUrl.QueueUrl,
+                MessageBody = JsonSerializer.Serialize(message),
+                MessageGroupId = "1"
+            };
+            await _sqs.SendMessageAsync(request);
+        }
+    }
+}
